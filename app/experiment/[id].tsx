bf2fcc740422e24dfe63e9aa7b79109b7ee902ff
Spoodies <kaleb.pick@gmail.com>
2026-03-13 15:47:40 +0100

    renamed predInt

--- app/experiment/[id].tsx
+++ app/experiment/[id].tsx
@@ -16,7 +16,7 @@ const STAT_OPTIONS = [
   { id: 'median', label: 'Median' },
   { id: 'mode', label: 'Mode' },
   { id: 'confint', label: 'Confint' },
-  { id: 'predint', label: 'Pred Interval' },
+  { id: 'predint', label: 'Predint' },
   { id: 'stddev', label: 'Stddev' },
   { id: 'entry_count', label: 'Entry Count' },
 ];
